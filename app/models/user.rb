2c2425bc33d118559175899257b1c9d91ff9d7f5
Gary <gary.gc.chiu@gmail.com>
2017-01-28 14:25:39 -0500

    fixed user registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   has_many :searches
-  
+
+
 end
--- db/migrate/20170128030933_devise_create_users.rb
+++ db/migrate/20170128030933_devise_create_users.rb
@@ -1,4 +1,5 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
+
   def change
     create_table :users do |t|
       ## Database authenticatable
@@ -6,8 +7,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -18,7 +19,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
-    
+
 
       ## Confirmable
       # t.string   :confirmation_token
